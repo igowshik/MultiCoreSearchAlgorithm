b014f8b651d369480e5b6466e96b6e61f5e09c38
Gowshik Raj <igowshik@yahoo.com>
2019-12-03 15:02:13 +0100

    SimplePrefixTrie search algorithm

---
+++ MultiCoreSearchAlgorithm/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MultiCoreSearchAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var trie = new SimplePrefixTrie(); // or new SimpleCompressedPrefixTrie();
+            trie.AddWord("hello");
+            trie.AddWord("iced");
+            trie.AddWord("i");
+            trie.AddWord("ice");
+            trie.AddWord("icecone");
+            trie.AddWord("dtgg");
+            trie.AddWord("hicet");
+            foreach (var w in trie.FindWordsMatchingPrefixesOf("icedtgg"))
+                Console.WriteLine(w);
+        }
+    }
+}
